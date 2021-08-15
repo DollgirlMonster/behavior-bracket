3be9cf0dceb708e16eedc37170bb92b9242478de
DollgirlMonster <girlcock@icloud.com>
2021-08-14 21:52:36 -0400

    Testing new angle calculation formula

--- berryimu.py
+++ berryimu.py
@@ -365,6 +365,8 @@ def getValues():
 
     #Convert Accelerometer values to degrees
     AccXangle =  atan(ACCx / (sqrt(ACCy ** 2) + (ACCz ** 2)))
+    AccXangle *= 180
+    AccXangle /= M_PI
     AccYangle =  (math.atan2(ACCz,ACCx)+M_PI)*RAD_TO_DEG
     AccZangle =  (math.atan2(ACCx,ACCy)+M_PI)*RAD_TO_DEG
 
