b0f150c90af4d299022921262a4703961ce5f5ad
DollgirlMonster <girlcock@icloud.com>
2021-08-24 13:15:52 -0400

    connect device to an access point

--- wifi.py
+++ wifi.py
@@ -1,4 +1,9 @@
+import os
+import subprocess
 import socket
+
+wpa_supplicant_conf = "/etc/wpa_supplicant/wpa_supplicant.conf"
+client_country = "US"  # TODO: optionify this
 
 def isConnected():
     """ Returns True if connected to the Internet """
@@ -11,10 +16,63 @@ def isConnected():
         pass
     return False
 
-def modeAP():
+def becomeAccessPoint():
     """ Switch the device to wireless access point mode """
     pass
 
-def modeClient(ssid, passkey):
-    """ Switch the device to wireless client mode """
-    pass
+def clientConnect(ssid, passkey):
+    """ 
+    Connect the device to a WiFi access point 
+    Returns IP address
+    """
+    # write wifi config to file
+    f = open('wifi.conf', 'w')
+    f.write(f'country={client_country}\n')
+    f.write('ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n')
+    f.write('update_config=1\n')
+    f.write('\n')
+    f.write('network={\n')
+    f.write('    ssid="' + ssid + '"\n')
+    f.write('    psk="' + passkey + '"\n')
+    f.write('}\n')
+    f.close()
+
+    # Move wifi config to system folder
+    cmd = 'mv wifi.conf ' + wpa_supplicant_conf
+    cmd_result = ""
+    cmd_result = os.system(cmd)
+    print cmd + " - " + str(cmd_result)
+
+    # restart wifi adapter
+    cmd = sudo_mode + 'ifdown wlan0'
+    cmd_result = os.system(cmd)
+    print cmd + " - " + str(cmd_result)
+
+    time.sleep(2)
+
+    cmd = sudo_mode + 'ifup wlan0'
+    cmd_result = os.system(cmd)
+    print cmd + " - " + str(cmd_result)
+
+    time.sleep(10)
+
+    cmd = 'iwconfig wlan0'
+    cmd_result = os.system(cmd)
+    print cmd + " - " + str(cmd_result)
+
+    cmd = 'ifconfig wlan0'
+    cmd_result = os.system(cmd)
+    print cmd + " - " + str(cmd_result)
+
+    # Get IP address
+    p = subprocess.Popen(['ifconfig', 'wlan0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    out, err = p.communicate()
+
+    ip_address = "<Not Set>"
+
+    for l in out.split('\n'):
+        if l.strip().startswith("inet addr:"):
+            ip_address = l.strip().split(' ')[1].split(':')[1]
+
+    return ip_address
