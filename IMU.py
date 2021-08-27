03e150514619fae57cfe152b81a22ab79f4a4737
DollgirlMonster <girlcock@icloud.com>
2021-08-27 00:22:30 -0400

    Merge LIS3MDL and LSM6DSL into IMU

--- IMU.py
+++ IMU.py
@@ -1,8 +1,82 @@
 import smbus
-bus = smbus.SMBus(0)
-from LSM6DSL import *
-from LIS3MDL import *
 import time
+
+bus = smbus.SMBus(0)
+
+# Set values for communicating with the LIS3MDL
+LIS3MDL_ADDRESS     = 0x1C
+
+LIS3MDL_WHO_AM_I    = 0x0F
+
+LIS3MDL_CTRL_REG1   = 0x20
+
+LIS3MDL_CTRL_REG2   = 0x21
+LIS3MDL_CTRL_REG3   = 0x22
+LIS3MDL_CTRL_REG4   = 0x23
+LIS3MDL_CTRL_REG5   = 0x24
+
+LIS3MDL_STATUS_REG  = 0x27
+
+LIS3MDL_OUT_X_L     = 0x28
+LIS3MDL_OUT_X_H     = 0x29
+LIS3MDL_OUT_Y_L     = 0x2A
+LIS3MDL_OUT_Y_H     = 0x2B
+LIS3MDL_OUT_Z_L     = 0x2C
+LIS3MDL_OUT_Z_H     = 0x2D
+
+LIS3MDL_TEMP_OUT_L  = 0x2E
+LIS3MDL_TEMP_OUT_H  = 0x2F
+
+LIS3MDL_INT_CFG     = 0x30
+LIS3MDL_INT_SRC     = 0x31
+LIS3MDL_INT_THS_L   = 0x32
+LIS3MDL_INT_THS_H   = 0x33
+ 
+# Set values for communicating with the LSM6DSL
+LSM6DSL_ADDRESS          =  0x6A
+
+LSM6DSL_WHO_AM_I         =  0x0F
+LSM6DSL_RAM_ACCESS       =  0x01
+LSM6DSL_CTRL1_XL         =  0x10
+LSM6DSL_CTRL8_XL         =  0x17
+LSM6DSL_CTRL2_G          =  0x11
+LSM6DSL_CTRL10_C         =  0x19
+LSM6DSL_TAP_CFG1         =  0x58
+LSM6DSL_INT1_CTR         =  0x0D
+LSM6DSL_CTRL3_C          =  0x12
+LSM6DSL_CTRL4_C          =  0x13
+
+LSM6DSL_STEP_COUNTER_L       =  0x4B
+LSM6DSL_STEP_COUNTER_H       =  0x4C
+
+LSM6DSL_OUTX_L_XL        =  0x28
+LSM6DSL_OUTX_H_XL        =  0x29
+LSM6DSL_OUTY_L_XL        =  0x2A
+LSM6DSL_OUTY_H_XL        =  0x2B
+LSM6DSL_OUTZ_L_XL        =  0x2C
+LSM6DSL_OUTZ_H_XL        =  0x2D
+
+LSM6DSL_OUT_L_TEMP       =  0x20
+LSM6DSL_OUT_H_TEMP       =  0x21
+
+LSM6DSL_OUTX_L_G         =  0x22
+LSM6DSL_OUTX_H_G         =  0x23
+LSM6DSL_OUTY_L_G         =  0x24
+LSM6DSL_OUTY_H_G         =  0x25
+LSM6DSL_OUTZ_L_G         =  0x26
+LSM6DSL_OUTZ_H_G         =  0x27
+
+LSM6DSL_TAP_CFG          =  0x58
+LSM6DSL_WAKE_UP_SRC      =  0x1B
+LSM6DSL_WAKE_UP_DUR      =  0x5C
+LSM6DSL_FREE_FALL        =  0x5D
+LSM6DSL_MD1_CFG          =  0x5E
+LSM6DSL_MD2_CFG          =  0x5F
+LSM6DSL_WAKE_UP_SRC      =  0x1B
+LSM6DSL_TAP_THS_6D       =  0x59
+LSM6DSL_INT_DUR2         =  0x5A
+LSM6DSL_WAKE_UP_THS      =  0x5B
+LSM6DSL_FUNC_SRC1        =  0x53
 
 def detectIMU():
     #Detect which version of BerryIMU is connected using the 'who am i' register
