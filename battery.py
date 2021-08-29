ec828bc6ed83033900eb6563f026fdd1971a4218
DollgirlMonster <girlcock@icloud.com>
2021-08-28 23:45:39 -0400

    removed loop that doesn't actually ever loop

--- battery.py
+++ battery.py
@@ -193,22 +193,22 @@ class INA219:
 def get_battery():
     # Create an INA219 instance.
     ina219 = INA219(addr=0x43)
-    while True:
-        bus_voltage = ina219.getBusVoltage_V()             # voltage on V- (load side)
-        shunt_voltage = ina219.getShuntVoltage_mV() / 1000 # voltage between V+ and V- across the shunt
-        current = ina219.getCurrent_mA()                   # current in mA
-        power = ina219.getPower_W()                        # power in W
-        p = (bus_voltage - 3)/1.2*100
-        if(p > 100):p = 100
-        if(p < 0):p = 0
+    
+    bus_voltage = ina219.getBusVoltage_V()             # voltage on V- (load side)
+    shunt_voltage = ina219.getShuntVoltage_mV() / 1000 # voltage between V+ and V- across the shunt
+    current = ina219.getCurrent_mA()                   # current in mA
+    power = ina219.getPower_W()                        # power in W
+    p = (bus_voltage - 3)/1.2*100
+    if(p > 100):p = 100
+    if(p < 0):p = 0
 
-        # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
-        # print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
-        # print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
+    # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
+    # print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
+    # print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
 
-        return {
-            "loadVoltage": bus_voltage, # measured in V
-            "current": current/1000,    # measured in A
-            "power": power,             # measured in W
-            "percent": p
-        }
+    return {
+        "loadVoltage": bus_voltage, # measured in V
+        "current": current/1000,    # measured in A
+        "power": power,             # measured in W
+        "percent": p
+    }
